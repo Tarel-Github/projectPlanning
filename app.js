2251215414c7607db8c3ec6f55cb34753f15205a
[Github <[Github>
2022-11-06 17:35:56 +0900

    구조조정

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const jwt = require("jsonwebtoken")      
-const app = express();
-const router = express.Router();
-const SECRET_KEY = `customized-secret-key`;
-const routes = require('./index');
-const cors = require("cors")                              //자신이 아닌 다른 도메인, 다른 프로토콜, 다른 포트에 있는 리소스를 요청하는 장치
+require('dotenv').config();                       //환경변수(.env)파일을 사용
+const express = require("express");               //익스프레스 사용 명령어
+//const jwt = require("jsonwebtoken");              //jwt 사용 명령어
+const cors = require("cors")                      //자신이 아닌 다른 도메인, 다른 프로토콜, 다른 포트에 있는 리소스를 요청하는 장치
 
-app.use(express.json());
-app.use(cookieParser());
+const router = require('./src/api/index');                //index 파일 가져옴
+//const connect = require("./schemas/index.js");   //몽구스를 위한 스키마 index 가져옴
+
+const app = express();                            //익스프레스를 app에 정의
+
+//connect();    //위에 선언한 connect를 사용
+
+
+app.use(express.json());                          //json 파일을 서버 정보 주고 받는 것에 사용한다.
 app.use(express.urlencoded({extended:false}));
-
-
-app.use(cors());
+app.use(cors());                                    //cors 사용
 app.use(express.static("static"))                   //html 파일을 불러오겠다는 뜻이다.
 
 // app.use(errorLogger); // Error Logger
 // app.use(errorHandler); // Error Handler
 
-app.use('/', routes); // 라우터 등록
+app.use('/', router); // 라우터 등록
 
-
-app.listen(4000, () => {
-    console.log("장비를 가동합니다");
+app.listen(process.env.PORT, () => {
+    console.log(process.env.PORT + "번 서버를 가동합니다");
+    console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━명령 대기중━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
   });
