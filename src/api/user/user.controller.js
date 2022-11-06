14175909ab458eab44c0eaf3473ad8a0b0325bbb
[Github <[Github>
2022-11-06 17:44:02 +0900

    ^-^

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -3,11 +3,18 @@ const UserService = require('./user.service');
 class UserController {
     userService = new UserService();
 
+    //회원가입
     signUp = async (req, res, next) =>{
 
     }
 
-    signIn = async (req, res, next) =>{
+    //로그인
+    login = async (req, res, next) =>{
+
+    }
+
+    //이메일 중복 확인
+    emailDup = async (req, res, next) =>{
 
     }
 
--- src/api/user/user.route.js
+++ src/api/user/user.route.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 const UserController = require('./user.controller');
 const userController = new UserController();
+
+//==아직은 사용하지 않는 기능들 =================================
+// const multerS3 = require('../middlewares/imageUploadMiddleware');
+// const multer = new multerS3();
+// const loginMiddleware = require('../middlewares/authLoginUserMiddleware');
+//==아직은 사용하지 않는 기능들 =================================
+
 // const auth = require('../middlewares/authMiddleware')    //미들웨어는 잠시 보류
 
 // 로그인/
-router.get('/:postId', userController.signUp);                   //회원가입 하기
-router.post('/:postId', userController.signIn);                //로그인 하기
+router.post('/signup', userController.signUp);                   //회원가입 하기
+router.post('/signup/emailDup', userController.emailDup);          //이메일 중복 체크
+router.post('/login', userController.login);                //로그인 하기
 
 module.exports = router;
