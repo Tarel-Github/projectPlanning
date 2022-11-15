533be47151bd169eedcf58ee356f760982de1428
[Github <[Github>
2022-11-15 11:12:56 +0900

    [기능] 작업중

--- src/api/project/project.repository.js
+++ src/api/project/project.repository.js
@@ -8,37 +8,36 @@ class ProjectRepository {
     getProjectAll = async (userId) =>{
         const getData = await project.getProject(userId)
         return getData
-    }
+    };
 
     //해당 프로젝트 자세히 보기
     getProjectDetail = async (userId, projectId) =>{
         const getData = await project.getProject(userId, projectId)
         return getData
-    }
+    };
 
-
-    ////////////////////////////////
     //새로온 프로젝트 작성하기
     postProject = async (userId, title) =>{
+        const date = new Date()
         let createdAt = date;
         let updatedAt = createdAt;
-        const postProject = Project.create({userId, title, createdAt, updatedAt})
+        let projectId =date.valueOf();
+        const postProject = Project.create({projectId, userId, title, createdAt, updatedAt})
         //const postProject = await this.projectRepository.postProject(userId);
         return postProject;
     };
-    ////////////////////////////////
     
     //기존 프로젝트 수정하기
     putProject = async (userId, projectId) => {
         const putProject = await this.projectRepository.putProject(userId);
         return putProject;
-    }
+    };
 
     //기존 프로젝트 삭제하기
     deleteProject = async (userId, projectId) => {
         const deleteProject = await this.projectRepository.deleteProject(userId);
         return deleteProject;
-    }
+    };
 
 }
 
