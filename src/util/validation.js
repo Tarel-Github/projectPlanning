af9f5eb607619e896c11807d37fcaa7d43556b19
[Github <[Github>
2022-11-18 14:48:13 +0900

    [기타] 이거 필요 없을 지도

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,45 +1,44 @@
-const joi = require('joi');
-
+const joi = require("joi");
+//이 파일 필요 없는것 같음
 module.exports = {
-    //회원가입시 체크사항
-    signupSchema: joi.object({
-        email: joi
-            .string()
-            .pattern(
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
-            )
-            .required(),
-        nickname: joi.string().min(2).max(10).required(),
-        password: joi
-            .string()
-            .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
-            .required(),
-        confirm: joi
-            .string()
-        .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
-        .required(),
-    }),
-    //로그인시 체크사항
-    loginSchema: joi.object({
-        email: joi
-            .string()
-            .pattern(
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
-            )
-            .required(),
-        password: joi
-            .string()
-            .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
-            .required(),
-    }),
-    //이메일 중복체크
-    emailDupSchema: joi.object({
-        email: joi
-            .string()
-            .pattern(
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
-            )
-            .required()
-    }),
-
+  //회원가입시 체크사항
+  signupSchema: joi.object({
+    email: joi
+      .string()
+      .pattern(
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+      )
+      .required(),
+    nickname: joi.string().min(2).max(10).required(),
+    password: joi
+      .string()
+      .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
+      .required(),
+    confirm: joi
+      .string()
+      .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
+      .required(),
+  }),
+  //로그인시 체크사항
+  loginSchema: joi.object({
+    email: joi
+      .string()
+      .pattern(
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+      )
+      .required(),
+    password: joi
+      .string()
+      .pattern(/^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/)
+      .required(),
+  }),
+  //이메일 중복체크
+  emailDupSchema: joi.object({
+    email: joi
+      .string()
+      .pattern(
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+      )
+      .required(),
+  }),
 };
