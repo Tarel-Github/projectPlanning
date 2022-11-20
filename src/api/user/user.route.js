052f4b39e5641d3ec12b52b42ef6ceee40c1b91e
[Github <[Github>
2022-11-21 01:50:22 +0900

    [기타] 주석 추가

--- src/api/user/user.route.js
+++ src/api/user/user.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const UserController = require("./user.controller");
 const userController = new UserController();
 
-const LoginMiddleware = require("../../middleware/LoginMiddleware");
+const LoginMiddleware = require("../../middleware/LoginMiddleware"); //이거쓰면 홈페이지에서 로그인이 안 됌
 
 //==아직은 사용하지 않는 기능들 =================================
 // const multerS3 = require('../middlewares/imageUploadMiddleware');
@@ -15,9 +15,9 @@ const LoginMiddleware = require("../../middleware/LoginMiddleware");
 // const auth = require('../middlewares/authMiddleware')    //미들웨어는 잠시 보류
 
 //경로 (/user)
-router.post("/signup", LoginMiddleware, userController.signUp); //회원가입 하기
+router.post("/signup", userController.signUp); //회원가입 하기
 router.post("/signup/dup", userController.dup); //아이디 중복 체크
-router.post("/login", LoginMiddleware, userController.login); //로그인 하기
+router.post("/login", userController.login); //로그인 하기
 
 //앞으로 추가해야할 기능들
 //카카오 로그인
