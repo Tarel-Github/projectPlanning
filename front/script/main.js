c398079d5e6197d7e324e2fd62eabcdb5e79ee36
[Github <[Github>
2022-11-20 23:48:23 +0900

    [front] 메인 페이지 작업

--- front/script/main.js
+++ front/script/main.js
@@ -1,29 +1,38 @@
-function myWorkList() {
-    console.log("내 작업 리스트")
+function myProjectList() {
     $.ajax({
         type: "GET",
         url: "/project/get",
         contentType: "json",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
         data: {},
         success: function (response) {
-            
-            let rows = response 
+            let rows = response.data
 
             for (let i = 0; i < rows.length; i++){      //게시글의 번호, 작성자, 제목, 내용, 날짜를 가져오며 그에 걸맞는 html을 출력한다.
-              let postsId = rows[i]['postsId']
-              let user = rows[i]['user']
+              let num = i+1;
+              let projectId = rows[i]['projectId']
               let title = rows[i]['title']
               let content = rows[i]['content']
-              let date = rows[i]['createdAt']
+              let createdAt = rows[i]['createdAt']
+              let cAt = createdAt.split("T")[0]
+              let updatedAt = rows[i]['updatedAt']
+              let uAt = updatedAt.split("T")[0]
               let temp_html = `                                  
-                                <tr>
-                                    <td style="width: 700px;" onclick="location.href='/api/posts/${postsId}'" value ="page move">${title}</td>
-                                    <td style="width: 150px;">${user}</td>
-                                    <td style="width: 250px;">${date}</td>
+                                <tr onclick="location.href='/project/get/${projectId}'" value ="page move">
+                                    <td style="width: 50px;">${num}</td>
+                                    <td style="width: 700px;">${title}</td>
+                                    <td style="width: 250px;">${cAt}</td>
+                                    <td style="width: 250px;">${uAt}</td>
                                 </tr>`
               $('#projectList').append(temp_html)                 
             }
 
         }
     })
+}
+
+function myProjectDetail(){
+
 }
