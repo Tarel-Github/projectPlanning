027a247abfb416edece1b3cc0b47e8c243c8af34
tarel <sms_stmax@naver.com>
2022-12-22 01:02:02 +0900

    [게임] 테트리스

--- front/game/tetris.js
+++ front/game/tetris.js
@@ -5,16 +5,58 @@ $(document).ready(function () {
 
 //시작하기 전에, 그리드 부터 까는 작업
 function grid() {
-    console.log("실행은 도ㅒㅆ는지")
     let grid
     for (let i = 1; i < 20; i++) {
         grid = `<tr id="grid${i}">`
         $(`#tetrisGrid`).append(grid)
         for (let j = 0; j <10; j++){
             let a =`<td id="[${i},${j}]"></td>`
-            console.log(a)
             $(`#grid${i}`).append(a);
         }
     }
 }
+
+let blockList =[]
+
+function startGame(){
+    const min = 0;
+    const max = 7;
+
+    if(blockList.length < 5){
+        getBlock(Math.floor(Math.random()*(max-min) + min));
+    }
+} 
   
+function getBlock(n){
+    console.log("-------------------------------")
+    console.log(n)
+    
+    for(let i = 0; i <5; i++) {
+        blockList.push(n) 
+    }
+}
+
+
+function Block_0_Box(){
+
+}
+function Block_1_Stick(){
+    
+}
+
+function Block_2_T(){
+    
+}
+
+function Block_3_S1(){
+    
+}
+function Block_4_S2(){
+    
+}
+function Block_5_L1(){
+    
+}
+function Block_6_L2(){
+    
+}
